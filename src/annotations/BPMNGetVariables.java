4166fec6d9dbd691db82722e8f29379e9cbded12
danilonunezgil <danilo.gil98@gmail.com>
2023-11-17 14:44:33 -0500

    library first version

---
+++ src/annotations/BPMNGetVariables.java
@@ -0,0 +1,14 @@
+
+package annotations;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.TYPE, ElementType.FIELD})
+public @interface BPMNGetVariables {
+    String[] variables() default "";
+}
